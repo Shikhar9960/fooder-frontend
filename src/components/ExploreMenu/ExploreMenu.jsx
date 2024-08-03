991dbc0f893de0516a388cee7189251a215e6009
Shikhar9960 <shikharjais5@gmail.com>
2024-08-03 18:27:01 +0530

    lst  commit

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,24 +1,50 @@
+// import React from 'react'
+// import './ExploreMenu.css'
+// import { menu_list } from "../../assets/assets"
+// import Home from '../../pages/Home/Home'
+
+// const ExploreMenu = ({ category, setCategory }) => {
+//     return (
+//         <div className='explore-menu' id='explore-menu'>
+//             <h1>explore our menu</h1>
+//             <p className='explore-menu-text'>choose from a diverse menu featuring a delectable array of dishes. our mission a to Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis, vero!     </p>
+//             <div className='explore-menu-list'>
+//                 {menu_list.map((item, index) => {
+//                     return (<div onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)} key={index} className='explore-menu-list-item'>
+//                         <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt="" />
+//                         <p>{item.menu_name}</p>
+//                     </div>)
+//                 })}
+//             </div>
+//             <hr />
+//         </div>
+//     )
+// }
+
+// export default ExploreMenu
+
 import React from 'react'
 import './ExploreMenu.css'
-import { menu_list } from "../../assets/assets"
-import Home from '../../pages/Home/Home'
+import { menu_list } from '../../assets/assets'
 
-const ExploreMenu = ({ category, setCategory }) => {
-    return (
-        <div className='explore-menu' id='explore-menu'>
-            <h1>explore our menu</h1>
-            <p className='explore-menu-text'>choose from a diverse menu featuring a delectable array of dishes. our mission a to Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis, vero!     </p>
-            <div className='explore-menu-list'>
-                {menu_list.map((item, index) => {
-                    return (<div onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)} key={index} className='explore-menu-list-item'>
-                        <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt="" />
-                        <p>{item.menu_name}</p>
-                    </div>)
-                })}
-            </div>
-            <hr />
+const ExploreMenu = ({category,setCategory}) => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+        <h1 className='h1e'>Explore our menu</h1>
+        <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes.</p>
+        <div className="explore-menu-list">
+            {menu_list.map((item, index)=>{
+                return (
+                    <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                        <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+                        <p className='item_menu'>{item.menu_name}</p>
+                    </div>
+                )
+            })}
         </div>
-    )
+        <hr />
+    </div>
+  )
 }
 
 export default ExploreMenu
